5cf887cb99db60d2126bef73cfdef81a4996e235
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-09 17:47:47 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ document.querySelector(".fas.fa-bars").addEventListener("click", function() {
     document.querySelector(".hamburger-menu").style.display = "block";
 })
 
+document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector(".hamburger-menu").style.display = "none";
+})
